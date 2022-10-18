17616bf99df71c2753fb8a65aff2e39fb5d11194
PHAIDI <phaidisamuel.ps@gmail.com>
2022-10-18 12:41:19 +0200

    routes

--- src/globals.ts
+++ src/globals.ts
@@ -1,9 +1,9 @@
 
 //REST API ENDPOINT
-export const GRAPH_HOSTNAME = "http://localhost:4242"
+export const GRAPH_HOSTNAME = "http://3.88.53.196:4242"
 
 //FILE TRANSFER ENDPOINT
-export const APP_HOSTNAME = "http://localhost:4241"
+export const APP_HOSTNAME = "http://3.88.53.196:4241"
 
 //SOCKETIO ENDPOINT
 export const SOCK_HOSTNAME = "http://ec2-54-234-88-147.compute-1.amazonaws.com"
