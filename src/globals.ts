93b2cdb42c1d8f187f657f80c642387bd68d3986
PHAIDI <phaidisamuel.ps@gmail.com>
2022-10-18 12:47:01 +0200

    dist

--- src/globals.ts
+++ src/globals.ts
@@ -9,6 +9,8 @@ export const APP_HOSTNAME = "http://3.88.53.196:4241"
 export const SOCK_HOSTNAME = "http://ec2-54-234-88-147.compute-1.amazonaws.com"
 
 //Apache FTP (SMTP,PDFRender,Frontend)
-export const APACHE_HOST = "http://3.88.53.196/Orientation_Final-BE/extra/";
+export const APACHE_HOST = "http://3.88.53.196/Orientation_Final-BE/extra"
+
+//New change
 
 
