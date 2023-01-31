92811f470328e2489e774b099fc71809e4426d9a
mpho.nyambeni-at-730528711512 <nyambenimphogiven@gmail.com>
2023-01-31 09:08:58 +0200

    diss

--- src/globals.ts
+++ src/globals.ts
@@ -1,15 +1,15 @@
 
 //REST API ENDPOINT
-export const GRAPH_HOSTNAME = "http://3.88.53.196:4242"
+export const GRAPH_HOSTNAME = "https://3.88.53.196:4242"
 
 //FILE TRANSFER ENDPOINT
-export const APP_HOSTNAME = "http://3.88.53.196:4241"
+export const APP_HOSTNAME = "https://3.88.53.196:4241"
 
 //SOCKETIO ENDPOINT
 export const SOCK_HOSTNAME = "http://ec2-54-234-88-147.compute-1.amazonaws.com"
 
 //Apache FTP (SMTP,PDFRender,Frontend)
-export const APACHE_HOST = "http://3.88.53.196/Orientation_Final-BE/extra"
+export const APACHE_HOST = "https://3.88.53.196/Orientation_Final-BE/extra"
 
 //New change
 //3.88.53.196/Orientation_Final-BE/extra
