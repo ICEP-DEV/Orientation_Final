e6c2aaaf3e2ba9fa2ab4b5e03fa8b49e327eadbf
mpho.nyambeni-at-730528711512 <nyambenimphogiven@gmail.com>
2023-04-10 13:31:13 +0200

    updated

--- src/globals.ts
+++ src/globals.ts
@@ -1,15 +1,15 @@
 
 //REST API ENDPOINT
-export const GRAPH_HOSTNAME = "https://api.onlinetutorientation.com"
+export const GRAPH_HOSTNAME = "http://localhost:3000"
 
 //FILE TRANSFER ENDPOINT
-export const APP_HOSTNAME = "https://api.onlinetutorientation.com"
+export const APP_HOSTNAME = "http://localhost:3000"
 
 //SOCKETIO ENDPOINT
 export const SOCK_HOSTNAME = "http://ec2-54-234-88-147.compute-1.amazonaws.com"
 
 //Apache FTP (SMTP,PDFRender,Frontend)
-export const APACHE_HOST = "https://api.onlinetutorientation.com/Orientation_Final-BE/extra"
+export const APACHE_HOST = "http://localhost/Orientation_Final-BE/extra"
 
 //New change
 //3.88.53.196/Orientation_Final-BE/extra
